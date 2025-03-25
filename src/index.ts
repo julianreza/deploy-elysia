3edf64ffa02921b6a099baaadc7f89ff1269961e
Reza Julian <rhezandroid@gmail.com>
2025-03-25 13:57:51 +0700

    deno 1

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ const app = new Elysia().get("/", () => "Hello Elysia").listen(process.env.PORT 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+export default app.fetch;
