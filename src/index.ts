19e1d18e1ea72654402dfd5ed0ecfbf173186647
Reza Julian <rhezandroid@gmail.com>
2025-03-25 13:56:06 +0700

    deno

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import { Elysia } from "elysia";
-import handler from '@elysiajs/node'
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(process.env.PORT ?? 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
-
-export default handler()
