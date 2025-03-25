082ce11a459bd731cde36993c9bcdd15b3f53e23
Reza Julian <rhezandroid@gmail.com>
2025-03-25 14:08:50 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,4 @@ import { Elysia } from "elysia";
 
 const app = new Elysia().get("/", () => "Hello Elysia");
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
-
 export default app.fetch;
