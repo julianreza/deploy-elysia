95f0531c30589c20486cb42d2be79a19da57c5f2
Reza Julian <rhezandroid@gmail.com>
2025-03-25 14:02:03 +0700

    fetch

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(process.env.PORT ?? 3000);
+const app = new Elysia().get("/", () => "Hello Elysia");
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
